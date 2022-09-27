d0e3be2c6078d3ff091e9f99a4b23cf678e22754
斎藤正大 <saima.ptw88@gmail.com>
2022-09-27 21:09:37 +0900

    update chpter 4

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,23 @@ int main()
 {
   auto start = std::chrono::system_clock::now();
 
-  int n = fibo(6);
-  std::cout << n << std::endl;
+  // int n = fibo(6);
+  // std::cout << n << std::endl;
 
+  const int fibo_count = 6;
+  std::vector<int> fibos(fibo_count + 1);
+  for (int i = 0; i <= fibo_count; ++i)
+  {
+    if (i == 0)
+      fibos[i] = 0;
+    else if (i == 1)
+      fibos[i] = 1;
+    else
+    {
+      fibos[i] = fibos[i - 1] + fibos[i - 2];
+    }
+  }
+  std::cout << "fibos: " << fibos[fibo_count] << std::endl;
   auto end = std::chrono::system_clock::now();
   auto dur = end - start;
   auto msec = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
