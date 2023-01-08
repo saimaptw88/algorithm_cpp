52f5c60bfefcc6f13fef067de9f6258e09e8ad51
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-01-08 14:52:45 +0900

    handle to cpplint

--- src/ch05/chapter5.h
+++ src/ch05/chapter5.h
@@ -1,4 +1,7 @@
 // Copyright 2022 saito
+#ifndef SRC_CH05_CHAPTER5_H_
+#define SRC_CH05_CHAPTER5_H_
+
 #include <algorithm>
 #include <functional>
 #include <iostream>
@@ -13,11 +16,14 @@ template <class T>
 void chmin(T &a, T b) {
   if (a > b) a = b;
 }
+
 void flog_move();
 void flog_move_deliver();
 long long recursion_search(int i, std::vector<long long> h,
                            std::vector<long long> *dp);
 }  // namespace relaxation
+
+
 namespace knapsack {
 template <class T>
 T chmax(T a, T b) {
@@ -27,6 +33,8 @@ T chmax(T a, T b) {
 
 void execute();
 }  // namespace knapsack
+
+
 namespace edit_distance {
 template <class T>
 void chmin(T &a, T b) {
@@ -35,6 +43,8 @@ void chmin(T &a, T b) {
 
 void execute();
 }  // namespace edit_distance
+
+
 namespace section_division {
 template <class T>
 void chmin(T &a, T b) {
@@ -43,6 +53,7 @@ void chmin(T &a, T b) {
 
 void execute();
 }  // namespace section_division
+
 
 void question_1();
 void question_2();
@@ -56,3 +67,5 @@ bool question_5(int N, int W, std::vector<int> a);
 void question_6();
 bool question_6(int N, int W, std::vector<int>a, std::vector<int>m);
 }  // namespace chapter_5
+
+#endif  // SRC_CH05_CHAPTER5_H_
