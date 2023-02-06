6d87b0eae2c2644f2b71253836ce9cec1d32f743
斎藤正大 <saima.ptw88@gmail.com>
2023-02-07 05:44:31 +0900

    add question 3 at chapter 5

--- src/dp/dp.cc
+++ src/dp/dp.cc
@@ -4,3 +4,29 @@ int hello() {
   std::cout << "hello" << std::endl;
   return 0;
 }
+
+void question5_3() {
+  std::random_device rnd;
+  const int N = rnd() % 10 + 3;
+  const int W = rnd() % 10 + 4;
+
+  std::vector<int> a(N, 0);
+  for (int i = 0; i < N; ++i) {
+    a[i] = rnd();
+  }
+
+  std::vector<std::vector<int>> dp;
+  dp.assign(N, std::vector<int>(W, 0));
+
+  for (int i = 0; i < N; ++i) {
+    for (int w = 0; w < W; ++w) {
+      if (i > 0) dp[i][w] = std::max(dp[i][w], dp[i - 1][w]);
+
+      if (w - a[i] > 0) {
+        dp[i][w] = std::max(dp[i][w], dp[i][w - a[i]] + a[i]);
+      }
+    }
+  }
+
+  std::cout << dp[N - 1][W - 1] << std::endl;
+}
--- src/dp/dp.hh
+++ src/dp/dp.hh
@@ -1,3 +1,7 @@
+#include <algorithm>
 #include <iostream>
+#include <random>
 
 int hello();
+
+void question5_3();
